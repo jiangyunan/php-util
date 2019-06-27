72ed32a1f878b738226ed59f93f1e9d0c924fc43
jiangyunan <jiangyunan@gmail.com>
2019-06-27 18:17:06 +0800

    Update helper.php

--- helper.php
+++ helper.php
@@ -206,3 +206,22 @@ if (!function_exists('sortByPinyin')) {
         });
     }
 }
+
+/**
+ * 删除emoji表情
+ * @param $str
+ * @return string|string[]|null
+*/
+if (!function_exists('filterEmoji')) {
+    function filterEmoji($str)
+    {
+        $str = preg_replace_callback(    //执行一个正则表达式搜索并且使用一个回调进行替换
+            '/./u',
+            function (array $match) {
+                return strlen($match[0]) >= 4 ? '' : $match[0];
+            },
+            $str);
+
+        return $str;
+    }
+}
